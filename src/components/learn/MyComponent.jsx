7851ec9b44470636f92c584deeea894bc4196cd8
Minh <mtruong909@gmail.com>
2025-06-10 11:51:39 +0700

    Use Fragment & add css to component

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,8 +1,21 @@
+import './style.css';
+
 const MyComponent = () => {
     return (
-        <div>
-            Minh & Tien Update
-        </div>
+        <>
+            <div>
+                Minh & Tien Update
+            </div>
+            <div className="child"
+                style={
+                    {
+                        borderRadius: "10px",
+                    }
+                }
+            >
+                Child Component
+            </div>
+        </>
     );
 }
 
