0b14d3a2426b1b35046652a6c4163dc2c4011937
Minh <mtruong909@gmail.com>
2025-06-11 17:15:20 +0700

    Use event listener on function & get input value by event.target.value

--- src/components/todo/TodoInput.jsx
+++ src/components/todo/TodoInput.jsx
@@ -2,10 +2,22 @@ const TodoInput = (props) => {
     const { addNewTodo } = props;
 
     // addNewTodo("Minh");
+
+    const handleClick = () => {
+        alert("Click me");
+    }
+
+    const handleOnChange = (name) => {
+        console.log("OnChange: ", name);
+    }
+
     return (
         <div className='todo-input'>
-            <input type="text" />
-            <button>Add</button>
+            <input type="text" onChange={(event) => handleOnChange(event.target.value)} />
+            <button
+                onClick={handleClick}>
+                Add
+            </button>
         </div>
     );
 }
