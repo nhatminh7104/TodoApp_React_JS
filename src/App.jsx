befdcb944b820ccd7039114758c99d45a5d9fbe0
Minh <mtruong909@gmail.com>
2025-06-13 23:32:03 +0700

    Define child Compoenent for App Component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import ReactLogo from './assets/react.svg';
 import { useState } from 'react';
 import Header from './components/layout/header';
 import Footer from './components/layout/footer';
+import { Outlet } from 'react-router-dom';
 
 const App = () => {
   const [tasks, setTasks] = useState([]);
@@ -40,6 +41,7 @@ const App = () => {
         <TodoData tasks={tasks} deleteTask={deleteTask} />
       </div>
 
+      <Outlet />
       <Footer />
     </div>
   );
