dae7598298ff44223a13ae76269a5cad9c9db2c8
Minh <mtruong909@gmail.com>
2025-06-19 19:36:43 +0700

    Add useState & function to get image file user upload, add preview div

--- src/components/user/detail.user.drawer.jsx
+++ src/components/user/detail.user.drawer.jsx
@@ -1,4 +1,5 @@
 import { Button, Drawer } from 'antd';
+import { useState } from 'react';
 
 const DetailUserDrawer = (props) => {
     const {
@@ -10,6 +11,26 @@ const DetailUserDrawer = (props) => {
         setDataDetail(null);
         setIsDetailOpen(false);
     }
+
+    const [selectedFile, setSelectedFile] = useState(null);
+    const [preview, setPreview] = useState(null);
+
+    const handleOnChangeFile = (e) => {
+        if (!e.target.files || e.target.files.length === 0) {
+            setSelectedFile(null);
+            setPreview(null);
+            return;
+        }
+
+        const file = e.target.files[0];
+        if (file) {
+            setSelectedFile(file);
+            setPreview(URL.createObjectURL(file));
+        }
+        console.log("User file preview: ", preview);
+    }
+
+    console.log("User file preview: ", preview);
 
     return (
         <>
@@ -33,17 +54,35 @@ const DetailUserDrawer = (props) => {
                         <p><strong>Phone:</strong> {dataDetail.phone}</p>
                         <br />
                         <strong>Avatar:</strong>
-                        <div>
-                            <img height={125} width={180} style={{ marginTop: 15 }}
+                        <div style={{
+                            marginTop: 10,
+                            height: 100,
+                            width: 150,
+                        }}>
+                            <img style={{ marginTop: 15, height: "100%", width: "100%", objectFit: "contain" }}
                                 src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${dataDetail.avatar}`}
                                 alt="user-avatar" />
                         </div>
-                        <div>
+
+                        <div style={{ marginTop: 35 }}>
                             <label htmlFor="btnUpload" style={{ display: "inline-block" }}>
                                 Upload Avatar
                             </label>
-                            <input type="file" id="btnUpload" hidden />
+                            <input type="file" id="btnUpload" hidden onChange={(e) => handleOnChangeFile(e)} />
                         </div>
+
+                        {preview &&
+                            <div style={{
+                                marginTop: 10,
+                                height: 100,
+                                width: 150,
+                            }}>
+                                <img style={{ marginTop: 15, height: "100%", width: "100%", objectFit: "contain" }}
+                                    src={preview}
+                                    alt="user-avatar" />
+                            </div>
+                        }
+
                     </>
                     : <p>There is no data for this user!</p>
                 }
