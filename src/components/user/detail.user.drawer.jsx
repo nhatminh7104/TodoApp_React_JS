d3f5c689c56e13386c6470211ff7cfbf84cb2e54
Minh <mtruong909@gmail.com>
2025-06-19 14:23:01 +0700

    Add user avatar and upload file button in user detail

--- src/components/user/detail.user.drawer.jsx
+++ src/components/user/detail.user.drawer.jsx
@@ -1,4 +1,4 @@
-import { Drawer } from 'antd';
+import { Button, Drawer } from 'antd';
 
 const DetailUserDrawer = (props) => {
     const {
@@ -14,6 +14,7 @@ const DetailUserDrawer = (props) => {
     return (
         <>
             <Drawer
+                width={'40vw'}
                 closable
                 destroyOnHidden
                 title={<p>Users Detail</p>}
@@ -21,13 +22,28 @@ const DetailUserDrawer = (props) => {
                 open={isDetailOpen}
                 onClose={closeUserForm}>
 
-
                 {dataDetail ?
                     <>
-                        <p>ID: {dataDetail._id}</p>
-                        <p>Full name: {dataDetail.fullName}</p>
-                        <p>Email: {dataDetail.email}</p>
-                        <p>Phone: {dataDetail.phone}</p>
+                        <p><strong>ID:</strong> {dataDetail._id}</p>
+                        <br />
+                        <p><strong>Full name:</strong> {dataDetail.fullName}</p>
+                        <br />
+                        <p><strong>Email:</strong> {dataDetail.email}</p>
+                        <br />
+                        <p><strong>Phone:</strong> {dataDetail.phone}</p>
+                        <br />
+                        <strong>Avatar:</strong>
+                        <div>
+                            <img height={125} width={180} style={{ marginTop: 15 }}
+                                src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${dataDetail.avatar}`}
+                                alt="user-avatar" />
+                        </div>
+                        <div>
+                            <label htmlFor="btnUpload" style={{ display: "inline-block" }}>
+                                Upload Avatar
+                            </label>
+                            <input type="file" id="btnUpload" hidden />
+                        </div>
                     </>
                     : <p>There is no data for this user!</p>
                 }
