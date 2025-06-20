c6e31bf1b3f7398a1ef327595e065fc0f73d6bb6
Minh <mtruong909@gmail.com>
2025-06-20 19:07:48 +0700

    create register form with antd

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,6 +1,64 @@
+import { Button, Input, Form } from "antd";
+
 const RegisterPage = () => {
+    const [form] = Form.useForm();
+
+    const onFinish = (values) => {
+        console.log("values: ", values);
+    }
+
     return (
-        <div>Register Page</div>
+        <>
+            <Form
+                layout="vertical"
+                form={form}
+                onFinish={onFinish}
+            // onFinishFailed={onFinishFailed}"
+            >
+                <div style={{
+                    width: "500px",
+                    display: "flex",
+                    margin: "50px auto",
+                    flexDirection: "column",
+                }}>
+                    <Form.Item
+                        label="Full Name"
+                        name="fullName"
+                    >
+                        <Input />
+                    </Form.Item>
+
+                    <Form.Item
+                        label="Email"
+                        name="email"
+                    >
+                        <Input />
+                    </Form.Item>
+
+                    <Form.Item
+                        label="Password"
+                        name="password"
+                    >
+                        <Input.Password />
+                    </Form.Item>
+
+                    <Form.Item
+                        label="Phone"
+                        name="phone"
+                    >
+                        <Input />
+                    </Form.Item>
+
+                    <div>
+                        <Button
+                            type="primary"
+                            onClick={() => form.submit()}>
+                            Register
+                        </Button>
+                    </div>
+                </div>
+            </Form>
+        </>
     );
 }
 
